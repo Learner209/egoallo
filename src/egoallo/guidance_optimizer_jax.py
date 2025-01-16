99c51beee748faff50be98de810cd8eb5083e23c
Learner209 <yufeiyang553@gmail.com>
2025-01-16 19:19:48 +0800

    (chore): update: fix bugs in guidanceoptimizer_jax.py for optimizing body sequences.

--- src/egoallo/guidance_optimizer_jax.py
+++ src/egoallo/guidance_optimizer_jax.py
@@ -849,16 +849,15 @@ def _optimize(
         # Get the relevant joint angles (knees and elbows)
         # Note: Adjusted indices for SMPL-H model joint ordering
         local_quats_aa = jaxlie.SO3(vals[pose]).log().flatten()
-        assert isinstance(local_quats_aa, Float[Tensor, "69"])
+        assert local_quats_aa.ndim == 1 and local_quats_aa.shape[-1] == 63
+        # assert isinstance(local_quats_aa, Float[Tensor, "63"])
 
         joint_angles = jnp.take(local_quats_aa, jnp.array([12, 15, 55, 58]))
         
         # Apply direction-specific penalties using same pattern as customloss.py
         signs = jnp.array([1., -1., -1., -1.])
-        return guidance_params.angle_prior_weight * jnp.sum(
-            jnp.exp(joint_angles * signs) ** 2,
-            axis=-1
-        )
+        # breakpoint()
+        return guidance_params.angle_prior_weight * (jnp.exp(joint_angles * signs) ** 2).flatten()
 
     vars_body_pose = _SmplhBodyPosesVar(jnp.arange(timesteps))
     vars_hand_pose = _SmplhSingleHandPosesVar(jnp.arange(timesteps * 2))
